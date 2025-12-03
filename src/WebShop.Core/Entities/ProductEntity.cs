4e961c5081b59535c659ff635d3f625415ab973f
majid-guluzade <majidguluzada@gmail.com>
2025-12-03 11:27:57 +0100

    Create Product Business Entity

--- src/WebShop.Core/Entities/ProductEntity.cs
+++ src/WebShop.Core/Entities/ProductEntity.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace WebShop.Core.Entities
 {
-    internal class ProductEntity
+    public class ProductEntity
     {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        public DateTime CreatedUtc { get; set; }
+        public string ImageUrl { get; set; }
+
+
+
     }
 }
