5f486e2b1e98473fe024e0199356b6b7ca3a2626
MK <marklonx9@gmail.com>
2025-12-04 14:27:54 +0100

    Add static files and controllers

---
+++ src/WebShop.MVC/Controllers/AccountController.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebShop.MVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ILogger<AccountController> _logger;
+
+        public AccountController(ILogger<AccountController> logger)
+        {
+            _logger = logger;
+        }
+
+        // GET: /Account/Index   -> Views/Account/Index.cshtml (page-account)
+        public IActionResult Account()
+        {
+            return View();
+        }
+
+        // GET: /Account/Login   -> Views/Account/Login.cshtml (page-login)
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: /Account/Login
+        // [HttpPost]
+        // [ValidateAntiForgeryToken]
+        //public IActionResult Login(/* LoginViewModel model */)
+        /*{
+            // TODO: validate credentials and sign user in
+            // if (!ModelState.IsValid) return View(model);
+
+            return RedirectToAction("Index", "Home");
+        }*/
+
+        // GET: /Account/Register   -> Views/Account/Register.cshtml (page-register)
+        // [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        // POST: /Account/Register
+        // [HttpPost]
+        // [ValidateAntiForgeryToken]
+        //public IActionResult Register(/* RegisterViewModel model */)
+        /*{
+            // TODO: create user account
+            // if (!ModelState.IsValid) return View(model);
+
+            return RedirectToAction("Login");
+        }*/
+
+        // GET: /Account/ForgotPassword   -> Views/Account/ForgotPassword.cshtml
+        // [HttpGet]
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+        // POST: /Account/ForgotPassword
+        // [HttpPost]
+        // [ValidateAntiForgeryToken]
+        // public IActionResult ForgotPassword(/* ForgotPasswordViewModel model */)
+        /*{
+            // TODO: send reset link
+            return RedirectToAction("ForgotPasswordConfirmation");
+        }*/
+
+        // GET: /Account/ForgotPasswordConfirmation
+        // [HttpGet]
+        public IActionResult Reset()
+        {
+            return View();
+        }
+
+        // GET: /Account/ResetPassword   -> Views/Account/ResetPassword.cshtml
+        // [HttpGet]
+        public IActionResult ResetPassword(string token, string email)
+        {
+            // pass token/email to view if needed
+            return View();
+        }
+
+        // POST: /Account/ResetPassword
+        // [HttpPost]
+        // [ValidateAntiForgeryToken]
+        public IActionResult ResetPassword(/* ResetPasswordViewModel model */)
+        {
+            // TODO: reset password
+            return RedirectToAction("Login");
+        }
+
+        // GET: /Account/PrivacyPolicy   -> Views/Account/Privacy.cshtml
+        // [HttpGet]
+        public IActionResult Privacy()
+        {
+            return RedirectToAction("Privacy", "Home");
+        }
+
+        // GET: /Account/Logout
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Logout()
+        {
+            // TODO: sign user out
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
+
---
+++ src/WebShop.MVC/Controllers/ShopController.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebShop.MVC.Controllers
+{
+    public class ShopController : Controller
+    {
+        private readonly ILogger<ShopController> _logger;
+        // TODO: IProductService, ICartService, IOrderService
+        // private readonly IProductService _products;
+        // private readonly ICartService _cart;
+        public ShopController(ILogger<ShopController> logger /*, IProductService products, ICartService cart */)
+        {
+            _logger = logger;
+            // _products = products;
+            // _cart = cart;
+        }
+
+        // Optional landing page -> Views/Shop/Index.cshtml (e.g., product list)
+        // [HttpGet]
+        public IActionResult ProductDetails()
+        {
+            // var model = _products.GetFeaturedOrAll();
+            return View();
+        }
+
+        // GET: /Shop/Product/123 or /Shop/Product?id=123
+        // Maps to Views/Shop/Product.cshtml (from shop-product-full.html)
+        // [HttpGet]
+        /* public IActionResult ProductDetails(int? id)
+        {
+            if (id == null)
+            {
+                // No id supplied; you can redirect to Index or show a not-found message
+                return RedirectToAction(nameof(Index));
+            }
+
+            // var product = _products.GetById(id.Value);
+            // if (product == null) return NotFound();
+            // return View(product);
+
+            return View();
+        }*/
+
+        // GET: /Shop/Cart
+        // Maps to Views/Shop/Cart.cshtml (from shop-cart.html)
+        // [HttpGet]
+        public IActionResult Cart()
+        {
+            // var cart = _cart.GetForUser(User);
+            // return View(cart);
+            return View();
+        }
+
+        // POST: /Shop/Cart/Add
+        // [HttpPost]
+        // [ValidateAntiForgeryToken]
+        public IActionResult AddToCart(int productId, int quantity = 1)
+        {
+            // if (quantity < 1) quantity = 1;
+            // _cart.Add(User, productId, quantity);
+            return RedirectToAction(nameof(Cart));
+        }
+
+        // POST: /Shop/Cart/Update
+        // [HttpPost]
+        // [ValidateAntiForgeryToken]
+        public IActionResult UpdateCart(int productId, int quantity)
+        {
+            // _cart.Update(User, productId, quantity);
+            return RedirectToAction(nameof(Cart));
+        }
+
+        // POST: /Shop/Cart/Remove
+        //[HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult RemoveFromCart(int productId)
+        {
+            // _cart.Remove(User, productId);
+            return RedirectToAction(nameof(Cart));
+        }
+
+        // GET: /Shop/Wishlist
+        // Maps to Views/Shop/Wishlist.cshtml (from shop-wishlist.html)
+        // [HttpGet]
+        public IActionResult Wishlist()
+        {
+            // var wishlist = _products.GetWishlist(User);
+            // return View(wishlist);
+            return View();
+        }
+
+        // POST: /Shop/Wishlist/Add
+        // [HttpPost]
+        // [ValidateAntiForgeryToken]
+        public IActionResult AddToWishlist(int productId)
+        {
+            // _products.AddToWishlist(User, productId);
+            return RedirectToAction(nameof(Wishlist));
+        }
+
+        // POST: /Shop/Wishlist/Remove
+        // [HttpPost]
+        // [ValidateAntiForgeryToken]
+        public IActionResult RemoveFromWishlist(int productId)
+        {
+            // _products.RemoveFromWishlist(User, productId);
+            return RedirectToAction(nameof(Wishlist));
+        }
+
+        // GET: /Shop/Checkout
+        // Maps to Views/Shop/Checkout.cshtml (from shop-checkout.html)
+        // [HttpGet]
+        public IActionResult Checkout()
+        {
+            // var model = _cart.GetCheckoutModel(User);
+            // return View(model);
+            return View();
+        }
+
+        // POST: /Shop/Checkout
+        // [HttpPost]
+        // [ValidateAntiForgeryToken]
+        // public IActionResult Checkout(/* CheckoutViewModel model */)
+        /*{
+            // if (!ModelState.IsValid) return View(model);
+            // var orderId = _cart.PlaceOrder(User, model);
+            // return RedirectToAction(nameof(OrderConfirmation), new { id = orderId });
+            return RedirectToAction(nameof(OrderConfirmation), new { id = 0 });
+        }*/
+
+        // GET: /Shop/OrderConfirmation/1001
+        // [HttpGet]
+        public IActionResult OrderConfirmation(int id)
+        {
+            // var order = _orders.GetByIdForUser(User, id);
+            // if (order == null) return NotFound();
+            // return View(order);
+            return View();
+        }
+    }
+}
