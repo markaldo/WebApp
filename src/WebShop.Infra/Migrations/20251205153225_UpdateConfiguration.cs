bccd5c1abae14d5671fcc5d515c09b81ceb35e30
Nam Long Vu <long.vu@euroclear.com>
2025-12-05 16:34:03 +0100

    Seperate configuration files

---
+++ src/WebShop.Infra/Migrations/20251205153225_UpdateConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebShop.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateConfiguration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 10);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "CreateUtc", "ImageUrl", "Price", "ProductCategoryId", "ProductName" },
+                values: new object[] { 10, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "~/assets/imgs/shop/9.jpg", 19.99m, 3, "Test" });
+        }
+    }
+}
--- src/WebShop.Infra/Persistence/Configuration/ProductConfig.cs
+++ src/WebShop.Infra/Persistence/Configuration/ProductConfig.cs
@@ -1,10 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using WebShop.Core.Entities;
 
-namespace WebShop.Infra.Persistence
+namespace WebShop.Infra.Persistence.Configuration
 {
-    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    public class ProductConfig : IEntityTypeConfiguration<Product>
     {
         public void Configure(EntityTypeBuilder<Product> modelBuilder)
         {
@@ -101,40 +106,6 @@ namespace WebShop.Infra.Persistence
                         ProductCategoryId = 3,
                         ImageUrl = "~/assets/imgs/shop/9.jpg"
                     }
-            );
-        }
-    }
-
-    public class ProductCategoryConfiguration : IEntityTypeConfiguration<ProductCategory>
-    {
-        public void Configure(EntityTypeBuilder<ProductCategory> modelBuilder)
-        {
-            // Primary Key
-            modelBuilder.HasKey(c => c.CatergoryId);
-
-            // Name: required, max length 50
-            modelBuilder.Property(c => c.CatergoryName)
-                        .IsRequired()
-                        .HasMaxLength(50);
-
-
-            // Seed categories
-            modelBuilder.HasData(
-                new ProductCategory
-                {
-                    CatergoryId = 1,
-                    CatergoryName = "Electronics"
-                },
-                new ProductCategory
-                {
-                    CatergoryId = 2,
-                    CatergoryName = "Furniture"
-                },
-                new ProductCategory
-                {
-                    CatergoryId = 3,
-                    CatergoryName = "Accessories"
-                }
             );
         }
     }
