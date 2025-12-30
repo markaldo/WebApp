9f3bdc7023e41380fb898575b0e91241a8d01766
Nam Long Vu <long.vu@euroclear.com>
2025-12-30 14:30:59 +0100

    Add Production migration

---
+++ src/WebShop.Infra/Migrations/20251230131211_ProductMigration.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebShop.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProductMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "OrderDate",
+                value: new DateTime(2025, 12, 29, 13, 12, 11, 334, DateTimeKind.Utc).AddTicks(8778));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "OrderDate",
+                value: new DateTime(2025, 12, 28, 13, 12, 11, 334, DateTimeKind.Utc).AddTicks(8803));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "OrderDate",
+                value: new DateTime(2025, 12, 29, 13, 12, 11, 334, DateTimeKind.Utc).AddTicks(8808));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreateUtc",
+                value: new DateTime(2025, 12, 22, 14, 12, 11, 336, DateTimeKind.Local).AddTicks(9208));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreateUtc",
+                value: new DateTime(2025, 12, 30, 14, 12, 11, 336, DateTimeKind.Local).AddTicks(9347));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreateUtc",
+                value: new DateTime(2025, 12, 27, 14, 12, 11, 336, DateTimeKind.Local).AddTicks(9356));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreateUtc",
+                value: new DateTime(2025, 12, 28, 14, 12, 11, 336, DateTimeKind.Local).AddTicks(9365));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreateUtc",
+                value: new DateTime(2025, 12, 24, 14, 12, 11, 336, DateTimeKind.Local).AddTicks(9376));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "CreateUtc",
+                value: new DateTime(2025, 12, 20, 14, 12, 11, 336, DateTimeKind.Local).AddTicks(9384));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "CreateUtc",
+                value: new DateTime(2025, 12, 28, 14, 12, 11, 336, DateTimeKind.Local).AddTicks(9391));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "CreateUtc",
+                value: new DateTime(2025, 12, 20, 14, 12, 11, 336, DateTimeKind.Local).AddTicks(9399));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "CreateUtc",
+                value: new DateTime(2025, 12, 20, 14, 12, 11, 336, DateTimeKind.Local).AddTicks(9407));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "OrderDate",
+                value: new DateTime(2025, 12, 29, 12, 34, 8, 421, DateTimeKind.Utc).AddTicks(2634));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "OrderDate",
+                value: new DateTime(2025, 12, 28, 12, 34, 8, 421, DateTimeKind.Utc).AddTicks(2642));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "OrderDate",
+                value: new DateTime(2025, 12, 29, 12, 34, 8, 421, DateTimeKind.Utc).AddTicks(2643));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreateUtc",
+                value: new DateTime(2025, 12, 22, 13, 34, 8, 422, DateTimeKind.Local).AddTicks(729));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreateUtc",
+                value: new DateTime(2025, 12, 30, 13, 34, 8, 422, DateTimeKind.Local).AddTicks(790));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreateUtc",
+                value: new DateTime(2025, 12, 27, 13, 34, 8, 422, DateTimeKind.Local).AddTicks(794));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreateUtc",
+                value: new DateTime(2025, 12, 28, 13, 34, 8, 422, DateTimeKind.Local).AddTicks(797));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreateUtc",
+                value: new DateTime(2025, 12, 24, 13, 34, 8, 422, DateTimeKind.Local).AddTicks(801));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "CreateUtc",
+                value: new DateTime(2025, 12, 20, 13, 34, 8, 422, DateTimeKind.Local).AddTicks(803));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "CreateUtc",
+                value: new DateTime(2025, 12, 28, 13, 34, 8, 422, DateTimeKind.Local).AddTicks(806));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "CreateUtc",
+                value: new DateTime(2025, 12, 20, 13, 34, 8, 422, DateTimeKind.Local).AddTicks(816));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "CreateUtc",
+                value: new DateTime(2025, 12, 20, 13, 34, 8, 422, DateTimeKind.Local).AddTicks(818));
+        }
+    }
+}
